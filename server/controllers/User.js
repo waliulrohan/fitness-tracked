9933785b3c668b4e9ee62fa87690c51aa0abf3c7
waliulohan <waliulislam1742@gmail.com>
2024-07-18 12:25:15 +0600

    done

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -4,10 +4,21 @@ import dotenv from "dotenv";
 import { createError } from "../error.js";
 import admin from "firebase-admin";
 dotenv.config()
-import secretFirestore from '../fitnesstracked-firebase-adminsdk-nyoqr-20cc8f868a.json' assert { type: "json" } ;
 
 admin.initializeApp({
-  credential: admin.credential.cert(secretFirestore)
+  credential: admin.credential.cert({
+    "type": "service_account",
+    "project_id": "fitnesstracked",
+    "private_key_id": process.env.KEY_ID,
+    "private_key": process.env.KEY,
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-nyoqr%40fitnesstracked.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  })
 });
 
 const db = admin.firestore();
