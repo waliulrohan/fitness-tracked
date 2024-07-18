e688857667873f5de737ffdefc8788f6e9b45329
waliulohan <waliulislam1742@gmail.com>
2024-07-18 10:53:05 +0600

    done

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); 
 
-console.log(process.env.JWT , 'muhha')
 
 app.use("/api/user/", UserRoutes);
 // error handler
