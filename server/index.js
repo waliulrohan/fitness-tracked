b1339362fcf38b8121e0d018aedc241a7c439c54
waliulohan <waliulislam1742@gmail.com>
2024-07-18 10:28:07 +0600

    done

--- server/index.js
+++ server/index.js
@@ -14,6 +14,8 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); 
 
+console.log(process.env.JWT , 'muhha')
+
 app.use("/api/user/", UserRoutes);
 // error handler
 app.use((err, req, res, next) => {
@@ -31,8 +33,6 @@ app.get("/", async (req, res) => {
     message: "Hello developers from Fitness Tracked",
   });
 });
-
-
 
 const startServer = async () => {
   try {
